459b2a1aa63a6272cd14ca88279cd2721ea84652
Sameeran Kunche <skunche@gmail.com>
2018-01-31 00:12:55 -0800

    Add some tooltips

--- frontend/js/components/outputs/annotated_description.js
+++ frontend/js/components/outputs/annotated_description.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import {red400, green400} from 'material-ui/styles/colors'
+import {red400, green400, blueGrey900, blueGrey100} from 'material-ui/styles/colors'
+import {Tooltip} from 'react-lightweight-tooltip';
+
+const greyRoundedStyle = {
+  content: {
+    backgroundColor: blueGrey900,
+    color: blueGrey100,
+  },
+  tooltip: {
+    backgroundColor: blueGrey900,
+    borderRadius: '10px',
+  },
+  arrow: {
+    borderTop: 'solid #263238 5px',
+  },
+};
 
 class AnnotatedDescription extends Component {
     render () {
         const word_importances = this.props.classifier.as_list[this.props.ageRange];
         let standardized_text_list = this.props.classifier.standardized_text.split(' ');
-        for (var i = 0; i < word_importances.length; i ++ ) {
+        for (var i = 0; i < word_importances.length; i++) {
             let wrd = word_importances[i][0];
             let importance = word_importances[i][1];
             standardized_text_list = standardized_text_list.map(
                 (origWord, idx) => {
                     if (origWord === wrd) {
                         return (
-                            <span style={{color: (importance >= 0 ? green400 : red400)}} key={idx}>
-                            {origWord}
-                            { idx !== standardized_text_list.length ? ' ' : ''}
+                            <span key={idx}>
+                                <Tooltip content={ importance.toString() } styles={ greyRoundedStyle }>
+                                    <span style={{color: (importance >= 0 ? green400 : red400)}}>{origWord}</span>
+                                </Tooltip>
                             </span>
                         );
                     }
@@ -22,7 +38,7 @@ class AnnotatedDescription extends Component {
                 }
             )
         }
-        return (<span>{ standardized_text_list }</span>);
+        return (<span>{ standardized_text_list.reduce((prev, curr) => [prev, ' ', curr]) }</span>);
     }
 }
 
